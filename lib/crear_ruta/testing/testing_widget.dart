c9017a1bd8acfd7eb448d189c804f8d6f76ba054
MrRozas <marozas@alumnos.uai.cl>
2023-11-21 10:48:25 -0300

    [Feature] Add testing with Webscrapping from Flask Python API

--- lib/crear_ruta/testing/testing_widget.dart
+++ lib/crear_ruta/testing/testing_widget.dart
@@ -1,12 +1,6 @@
-import '/flutter_flow/flutter_flow_theme.dart';
-import '/flutter_flow/flutter_flow_util.dart';
-import '/flutter_flow/flutter_flow_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-import 'testing_model.dart';
-export 'testing_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class TestingWidget extends StatefulWidget {
   const TestingWidget({Key? key}) : super(key: key);
@@ -16,179 +10,142 @@ class TestingWidget extends StatefulWidget {
 }
 
 class _TestingWidgetState extends State<TestingWidget> {
-  late TestingModel _model;
-
-  final scaffoldKey = GlobalKey<ScaffoldState>();
-
-  @override
-  void initState() {
-    super.initState();
-    _model = createModel(context, () => TestingModel());
-
-    _model.textController ??= TextEditingController();
-    _model.textFieldFocusNode ??= FocusNode();
-  }
-
-  @override
-  void dispose() {
-    _model.dispose();
-
-    super.dispose();
-  }
+  TextEditingController _textController = TextEditingController();
+  List<String> _savedAddresses = [];
+  bool _loading = false;
+  List<String> _resultAddresses = [];
 
   @override
   Widget build(BuildContext context) {
-    if (isiOS) {
-      SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(
-          statusBarBrightness: Theme.of(context).brightness,
-          systemStatusBarContrastEnforced: true,
-        ),
-      );
-    }
-
-    return GestureDetector(
-      onTap: () => _model.unfocusNode.canRequestFocus
-          ? FocusScope.of(context).requestFocus(_model.unfocusNode)
-          : FocusScope.of(context).unfocus(),
-      child: Scaffold(
-        key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: SafeArea(
-          top: true,
-          child: Padding(
-            padding: EdgeInsetsDirectional.fromSTEB(16.0, 16.0, 16.0, 16.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 16.0, 0.0),
-                  child: TextFormField(
-                    controller: _model.textController,
-                    focusNode: _model.textFieldFocusNode,
-                    obscureText: false,
-                    decoration: InputDecoration(
-                      labelText: 'Enter data',
-                      hintText: 'Type here...',
-                      hintStyle: FlutterFlowTheme.of(context).bodyLarge,
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: FlutterFlowTheme.of(context).primary,
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0x00000000),
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0x00000000),
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      focusedErrorBorder: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Color(0x00000000),
-                          width: 2.0,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0),
-                      ),
-                      contentPadding: EdgeInsetsDirectional.fromSTEB(
-                          20.0, 24.0, 20.0, 24.0),
-                    ),
-                    style: FlutterFlowTheme.of(context).bodyMedium,
-                    validator:
-                        _model.textControllerValidator.asValidator(context),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(0.0, 24.0, 0.0, 0.0),
-                  child: FFButtonWidget(
-                    onPressed: () {
-                      print('Button pressed ...');
-                    },
-                    text: 'Save Data',
-                    options: FFButtonOptions(
-                      width: double.infinity,
-                      height: 55.0,
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                      iconPadding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                      color: FlutterFlowTheme.of(context).primary,
-                      textStyle:
-                          FlutterFlowTheme.of(context).titleMedium.override(
-                                fontFamily: 'Readex Pro',
-                                color: Colors.white,
-                              ),
-                      elevation: 2.0,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsetsDirectional.fromSTEB(16.0, 0.0, 16.0, 0.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.max,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Saved Data:',
-                        style: FlutterFlowTheme.of(context)
-                            .headlineMedium
-                            .override(
-                              fontFamily: 'Outfit',
-                              color: FlutterFlowTheme.of(context).primaryText,
-                            ),
-                      ),
-                      Text(
-                        'No data saved yet',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Readex Pro',
-                              color: FlutterFlowTheme.of(context).secondaryText,
-                            ),
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding:
-                      EdgeInsetsDirectional.fromSTEB(16.0, 24.0, 16.0, 0.0),
-                  child: FFButtonWidget(
-                    onPressed: () {
-                      print('Button pressed ...');
-                    },
-                    text: 'Finish',
-                    options: FFButtonOptions(
-                      width: double.infinity,
-                      height: 55.0,
-                      padding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                      iconPadding:
-                          EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
-                      color: FlutterFlowTheme.of(context).primary,
-                      textStyle:
-                          FlutterFlowTheme.of(context).titleMedium.override(
-                                fontFamily: 'Readex Pro',
-                                color: Colors.white,
-                              ),
-                      elevation: 2.0,
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                  ),
-                ),
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Testing Widget'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: _textController,
+              decoration: InputDecoration(
+                labelText: 'Ingrese la dirección (dirección y comuna)',
+                hintText: 'Type here...',
+              ),
             ),
-          ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _savedAddresses.add(_textController.text);
+                  _textController.clear();
+                });
+              },
+              child: Text('Guardar dirección'),
+            ),
+            SizedBox(height: 16.0),
+            Text(
+              'Direcciones guardadas:',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _savedAddresses.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_savedAddresses[index]),
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Visibility(
+              visible: !_loading,
+              child: ElevatedButton(
+                onPressed: () {
+                  _fetchAndDisplayData(_savedAddresses);
+                },
+                child: Text('Finish'),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            if (_loading) CircularProgressIndicator(),
+            if (_resultAddresses.isNotEmpty)
+              Text(
+                'Direcciones obtenidas:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            if (_resultAddresses.isNotEmpty)
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _resultAddresses.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(_resultAddresses[index]),
+                    );
+                  },
+                ),
+              ),
+          ],
         ),
       ),
     );
   }
+
+  // Construir la URL con las entradas
+  String _buildUrl(List<String> addresses) {
+    String url = 'http://127.0.0.1:5000/api?';
+    for (String entry in addresses) {
+      url += 'Query=${Uri.encodeComponent(entry)}&';
+    }
+    return url.substring(0, url.length - 1); // Eliminar el último '&'
+  }
+
+  // Realizar la solicitud HTTP y mostrar los resultados
+  Future<void> _fetchAndDisplayData(List<String> addresses) async {
+    setState(() {
+      _loading = true;
+      _resultAddresses.clear(); // Limpiar las direcciones anteriores
+    });
+
+    final url = _buildUrl(addresses);
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final decoded = json.decode(response.body);
+        final direcciones = decoded['Direcciones'] as List;
+
+        // Muestra los datos en orden
+        for (var data in direcciones) {
+          final direccion = data['Direccion'];
+          print('Dirección: $direccion');
+          setState(() {
+            _resultAddresses.add(direccion);
+          });
+        }
+      } else {
+        print('Error en la solicitud HTTP: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error al realizar la solicitud HTTP: $e');
+    } finally {
+      setState(() {
+        _loading = false;
+      });
+    }
+  }
 }
+
+void main() {
+  runApp(MaterialApp(
+    home: TestingWidget(),
+  ));
+}
+
